10a54ba4823b54a37187face8428b702efc72576
Pashaie <pashaie@gmail.com>
2024-05-06 18:54:47 +0330

    windows service

---
+++ WindowsService/Service1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer();
+            timer.Interval = 1000 * 10; //10sec
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            File.AppendAllText(Path.Combine(AppContext.BaseDirectory, "log.txt"), $"{DateTime.Now} - My Log\n");
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+        }
+    }
+}
